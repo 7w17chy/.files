4177e212b389ca0303aa15f003d17827f7971d30
Max Nerius <nermax03@gmail.com>
2023-01-08 22:25:33 +0100

    add lazygit and port fish config

--- home.nix
+++ home.nix
@@ -9,11 +9,41 @@
   # packages to install
   home.packages = with pkgs; [
     bat
+    lazygit
   ];
 
-  # porting over dotfiles:
+  # emacs config
   home.file.".emacs.d/init.el".source = ./init.el;
   home.file.".emacs.d/settings.org".source = ./settings.org;
+
+  programs.git = {
+    enable = true;
+    userName = "Max Nerius";
+    userEmail = "nermax03@gmail.com";
+  };
+
+  programs.fish.enable = true;
+  programs.fish.plugins = [
+    {
+      name = "z";
+      src = pkgs.fetchFromGitHub {
+        owner = "jethrokuan";
+        repo = "z";
+        rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+        sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+      };
+    }
+  ];
+  programs.fish.interactiveShellInit = ''
+set -l nix_shell_info (
+  if test -n "$IN_NIX_SHELL"
+    echo -n "<nix-shell> "
+  end
+)
+  '';                        
+  programs.fish.shellAbbrs = {
+    lg = "lazygit";
+  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
