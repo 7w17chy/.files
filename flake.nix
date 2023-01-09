3f5c14be3bcabc6552cb4e2104d6385fe54e0886
Max Nerius <nermax03@gmail.com>
2023-01-09 20:52:37 +0100

    first barebones flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,36 @@
 {
   description = "thulis' NixOS configuration.";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+    home-manager = "github:nix-community/home-manager/release-22.11";
+    # make home-manager use our version of nixpkgs
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = { self, nixpkgs, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      workstation = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./workstation/configuration.nix
+        ];
+      };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+      thinkpad = lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./thinkpad/configuration.nix
+        ];
+      };
+    };
   };
 }
