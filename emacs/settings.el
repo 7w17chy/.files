f6a30b62ec87dde41b11f566ab8a828e7da1b846
Max Nerius <nermax03@gmail.com>
2023-01-10 15:02:19 +0100

    emacs: direnv for flexible dev tools access

--- emacs/settings.el
+++ emacs/settings.el
@@ -18,6 +18,10 @@
   :config
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
+(use-package direnv
+  :config
+  (direnv-mode))
+
 (setq frame-inhibit-implied-resize nil)
 
 (use-package gruvbox-theme
@@ -269,68 +273,6 @@
   (lsp-ui-sideline-enable nil)
   (lsp-ui-sideline-enable nil)
   (lsp-ui-doc-enable nil))
-
-(load "odin-mode")
-(add-to-list 'auto-mode-alist '("\\.odin\\'" . odin-mode))
-
-(add-to-list 'load-path "/home/thulis/devel/jakt/editors/emacs")
-(autoload 'jakt-mode "jakt-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.jakt\\'" . jakt-mode))
-
-(use-package rustic
-  :ensure
-  :bind (:map rustic-mode-map
-	      ("M-j" . lsp-ui-imenu)
-	      ("M-?" . lsp-find-references)
-	      ("C-c C-c l" . flycheck-list-errors)
-	      ("C-c C-c a" . lsp-execute-code-action)
-	      ("C-c C-c r" . lsp-rename)
-	      ("C-c C-c q" . lsp-workspace-restart)
-	      ("C-c C-c Q" . lsp-workspace-shutdown)
-	      ("C-c C-c s" . lsp-rust-analyzer-status))
-  :config
-  ;; uncomment for less flashiness
-  (setq lsp-eldoc-hook nil)
-  (setq lsp-enable-symbol-highlighting nil)
-  (setq lsp-signature-auto-activate nil)
-
-  ;; comment to disable rustfmt on save
-  (setq rustic-format-on-save t)
-  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
-
-(defun rk/rustic-mode-hook ()
-  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
-  ;; save rust buffers that are not file visiting. Once
-  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
-  ;; no longer be necessary.
-  (when buffer-file-name
-    (setq-local buffer-save-without-query t))
-  (add-hook 'before-save-hook 'lsp-format-buffer nil t))
-
-(use-package lsp-mode
-  :ensure
-  :commands lsp
-  :custom
-  ;; what to use when checking on-save. "check" is default, I prefer clippy
-  (lsp-rust-analyzer-cargo-watch-command "clippy")
-  (lsp-idle-delay 0.6)
-  ;; enable / disable the hints as you prefer:
-  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
-  (lsp-rust-analyzer-display-chaining-hints t)
-  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
-  (lsp-rust-analyzer-display-closure-return-type-hints t)
-  (lsp-rust-analyzer-display-reborrow-hints nil)
-  ;; disable flashy noise
-  (lsp-lens-enable nil)
-  (lsp-headerline-breadcrumb-enable nil)
-  :config
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-(use-package clojure-mode
-  :ensure t)
-(use-package cider
-  :after clojure-mode
-  :ensure t)
 
 ;  (use-package auctex
 ;    :ensure t)
